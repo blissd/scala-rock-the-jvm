2968e6510243c014f5630e0f988879bba66695d7
David <vivid.map6767@davidbliss.me>
2024-07-15 12:36:52 +0100

    Pattern matching 3

---
+++ src/main/scala/com/rockthejvm/part4power/PatternsEverywhere.scala
@@ -0,0 +1,34 @@
+package com.rockthejvm.part4power
+
+object PatternsEverywhere {
+
+  // big idea #1: catches are actually MATCHES
+  val potentialFailure = try {
+    // code
+  } catch {
+    case e: RuntimeException => "runtime ex"
+    case e: NullPointerException => "npe"
+    case _ => "some other exception"
+  }
+
+  // big idea #2: for comprehensions (generators) are based on pattern matching
+  val aList = List(1,2,3,4)
+  val evenNumbers = for {
+    n <- aList if n % 2 == 0
+  } yield n
+
+  val tuples = List((1,2), (3,4))
+  val filterTuples = for {
+    (first, second) <- tuples if first < 3
+  } yield second * 100
+
+  // big idea #3: new syntax (python-like)
+  val aTuple = (1,2,3)
+  val (a, b,c) = aTuple
+
+  val head :: tail = tuples
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
